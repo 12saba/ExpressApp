eee588e45e11ffed8e6ced8f4986900ce14002e8
12saba <sabahatlaitf126@gmail.com>
2024-08-02 12:43:27 +0500

    08/02/24 create Rest Api

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+var routes = require("express").Router();
+routes.get("/create", (req, res) => {
+  return res.send("crreate user API");
+});
+//upsert
+routes.get("/read", (req, res) => {
+  return res.send("crreate user READ API");
+});
+//delete
+routes.get("/delete", (req, res) => {
+  return res.send("crreate user DELETE API");
+});
+//update
+routes.get("/update", (req, res) => {
+  return res.send("crreate user UPDATE API");
 });
 
-module.exports = router;
+module.exports = routes;
